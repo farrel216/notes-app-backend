afcf03e40a4594bcc8e26591b8ca79dc846192b0
Farrel Andhika <farrel.arp@gmail.com>
2024-08-16 13:44:41 +0700

    export notes implemented

--- src/server.js
+++ src/server.js
@@ -14,6 +14,10 @@ const AuthenticationsValidator = require("./validator/authentications");
 const collaborations = require("./api/collaborations");
 const CollaborationsService = require("./services/postgres/CollaborationsService");
 const CollaborationsValidator = require("./validator/collaborations");
+// Exports
+const _exports = require("./api/exports");
+const ProducerService = require("./services/rabbitmq/ProducerService");
+const ExportsValidator = require("./validator/exports");
 require("dotenv").config();
 
 const init = async () => {
@@ -83,6 +87,13 @@ const init = async () => {
         collaborationsService,
         notesService,
         validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        validator: ExportsValidator,
       },
     },
   ]);
